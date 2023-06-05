2dcea8d69ae1c555770ef131dd01726de98383dc
Giovanna de Souza Silva <71770652+soueuagih@users.noreply.github.com>
2023-06-05 20:50:11 -0300

    Atualizar o Prova.c

--- Prova.c
+++ Prova.c
@@ -1,67 +1,96 @@
-class Node: 
-     def __init__(self, key): 
-         self.key = key 
-         self.left = None 
-         self.right = None 
-  
- def insert(root, key): 
-     if root is None: 
-         return Node(key) 
-     if key < root.key: 
-         root.left = insert(root.left, key) 
-     else: 
-         root.right = insert(root.right, key) 
-     return root 
-  
- def pre_order(root): 
-     if root is not None: 
-         print(root.key, end=" ") 
-         pre_order(root.left) 
-         pre_order(root.right) 
-  
- def in_order(root): 
-     if root is not None: 
-         in_order(root.left) 
-         print(root.key, end=" ") 
-         in_order(root.right) 
-  
- def post_order(root): 
-     if root is not None: 
-         post_order(root.left) 
-         post_order(root.right) 
-         print(root.key, end=" ") 
-  
- # Read the number of test cases 
- num_test_cases = int(input()) 
-  
- # Process each test case 
- for case in range(1, num_test_cases + 1): 
-     # Read the number of numbers in the set 
-     num_numbers = int(input()) 
-  
-     # Read the numbers and build the BST 
-     numbers = list(map(int, input().split())) 
-     root = None 
-     for number in numbers: 
-         root = insert(root, number) 
-  
-     # Print the test case number 
-     print(f"Case {case}:") 
-  
-     # Print the pre-order traversal 
-     print("Pre.: ", end="") 
-     pre_order(root) 
-     print() 
-  
-     # Print the in-order traversal 
-     print("In..: ", end="") 
-     in_order(root) 
-     print() 
-  
-     # Print the post-order traversal 
-     print("Post: ", end="") 
-     post_order(root) 
-     print() 
-  
-     # Print a blank line after each test case 
-     print()
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int key;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int key) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int key) {
+    if (root == NULL)
+        return createNode(key);
+
+    if (key < root->key)
+        root->left = insert(root->left, key);
+    else
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+void preOrder(Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->key);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void inOrder(Node* root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        printf("%d ", root->key);
+        inOrder(root->right);
+    }
+}
+
+void postOrder(Node* root) {
+    if (root != NULL) {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->key);
+    }
+}
+
+void freeTree(Node* root) {
+    if (root != NULL) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
+}
+
+int main() {
+    int num_test_cases;
+    scanf("%d", &num_test_cases);
+
+    for (int case_num = 1; case_num <= num_test_cases; case_num++) {
+        int num_numbers;
+        scanf("%d", &num_numbers);
+
+        Node* root = NULL;
+        for (int i = 0; i < num_numbers; i++) {
+            int number;
+            scanf("%d", &number);
+            root = insert(root, number);
+        }
+
+        printf("Case %d:\n", case_num);
+        printf("Pre.: ");
+        preOrder(root);
+        printf("\n");
+
+        printf("In..: ");
+        inOrder(root);
+        printf("\n");
+
+        printf("Post: ");
+        postOrder(root);
+        printf("\n");
+
+        printf("\n");
+
+        freeTree(root);
+    }
+
+    return 0;
+}
